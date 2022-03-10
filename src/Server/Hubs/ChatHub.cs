ba1e2e4d1c169d4a4891d39c1e4b3541b878542c
ecabigting <ericcabigting@outlook.com>
2022-03-10 16:41:16 +0400

    update main hub for basic code for connecting and sending msg

--- src/Server/Hubs/ChatHub.cs
+++ src/Server/Hubs/ChatHub.cs
@@ -1,16 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
-namespace Server.Hubs
+namespace cabzchat.Server.Hubs
 {
     public class ChatHub : Hub
     {
         public async Task SendMessage(string msg, string username)
         {
             await Clients.All.SendAsync("GetThatMessage",username,msg);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await SendMessage("","User Connected!");
+
+            await base.OnConnectedAsync();
         }
     }
 }
