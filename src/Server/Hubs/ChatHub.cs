7982f11399a44df3ff44e7adb5ccdb833ea7368a
ecabigting <ericcabigting@outlook.com>
2022-03-16 19:03:53 +0400

    - cleanout boiler plate code from dotnet
    - update code to handle username shown when user enter and leaving the chat

--- src/Server/Hubs/ChatHub.cs
+++ src/Server/Hubs/ChatHub.cs
@@ -4,14 +4,24 @@ namespace cabzchat.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        // dirty quick solution
+    private static Dictionary<string, string> Users = new Dictionary<string, string>();
         public async Task SendMessageToChat(string msg, string username)
         {
             await Clients.All.SendAsync("GetThatMessage",username,msg);
         }
 
+        public override async Task OnDisconnectedAsync(Exception? ex)
+        {
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await SendMessageToChat(string.Empty,$"{username} left the chat!"); 
+        }
+
         public override async Task OnConnectedAsync()
         {
-            await SendMessageToChat("","User Connected!");
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId,username);
+            await SendMessageToChat(string.Empty,$"{username} entered the chat!");
 
             await base.OnConnectedAsync();
         }
